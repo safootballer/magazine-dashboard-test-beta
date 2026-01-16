83cb5ae811e4cae955e519ea9be1265cde024bb9
safootballer <163460915+safootballer@users.noreply.github.com>
2026-01-16 18:22:42 +0500

    Update app14.py

--- app14.py
+++ app14.py
@@ -5,9 +5,8 @@ import json
 import hashlib
 from datetime import datetime
 from dotenv import load_dotenv
-
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.schema import Document
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_core.documents import Document
 from langchain_community.vectorstores import InMemoryVectorStore
 from langchain_openai import OpenAIEmbeddings, ChatOpenAI
 from langchain.chains import RetrievalQA
